504e3ca3c013d1ec43763396f3b50ba9f61f1849
DESKTOP-V0H394L\User <e107ti@etec.sp;gov;br>
2023-03-10 22:10:07 -0300

    exercício resolvido

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Pastorzinho");
+Console.WriteLine("do, ré,mi,fa,fa,fa");
+Console.WriteLine("do,ré,do,ré,ré,ré");
+Console.WriteLine("do,sol,fa,mi,mi,mi");
+Console.WriteLine("do,ré,mi,fá,fá,fá");
